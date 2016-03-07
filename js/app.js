954cfce7ca83096cadaa995eebfe1f2f22c64dfc
DGriffing <diana.griffing@gmail.com>
2016-03-07 10:44:59 -0800

    hail mary attempt at modules

--- js/app.js
+++ js/app.js
@@ -1,63 +1,30 @@
 'use strict';
 
 angular.module('myApp', [])
-.controller('ConceptCtrl', ['$scope', '$http', function($scope, $http) {
-	
-	$scope.sortBy = 'title';
+.controller('SectionCtrl', ['$scope', '$http', function($scope, $http) {
 
+	// Connects data on Concepts
 	$http.get('data/concepts.json').then(function(response){
 		console.log(response.data);
 		$scope.concepts = response.data;
-	})
+	});
+
+	// Connects Concept Detail Modules to the information in data
+	$scope.conceptInfo = function(obj){
+		console.log(obj)
+		$scope.selectedConcept = obj;
+		var modalInstance = $uibModal.open({
+			templateUrl: 'concept.html',
+			controller: 'ConceptCtrl',
+			scope: $scope
+		});
+	}
 }])
 
-
-
-
-
-
-
-
-// .config(function($stateProvider, $urlRouterProvider){
-// 	$stateProvider
-
-// 		.state('home', {
-// 			url: '/',
-// 			templateUrl: 'index.html',
-// 			controller: 'HomeCtrl'
-// 		})
-
-// 		.state('memberBios', {
-// 			url: '/concept-{id}',
-// 			templateUrl: 'concept.html',
-// 			controller: 'ConceptCtrl'
-// 		})
-// })
-
-// .controller('HomeCtrl', ['$scope', '$http', '$uibModal', '$filter', '$stateParams', function($scope, $http, $uibModal, $filter, $stateParams) {
+.controller('ConceptCtrl', ['$scope', '$http', '$uibModalInstance', function($scope, $http, $uibModalInstance) { 
     
-//     // Connects to Concept Data
-//     $http.get('data/concepts.json').then(function(response) {
-//         $scope.concepts = response.data;
-//     });  
-
-//     // Connects Biographies to the information in data
-//     $scope.conceptInfo = function(obj) {
-//         console.log(obj);
-//         $scope.selectedConcept = obj;
-//         var modalInstance = $uibModal.open({
-//             templateUrl: 'concept.html',
-//             controller: 'ConceptCtrl',
-//             scope: $scope
-//         });
-//     }
-// }])
-
-
-// .controller('ConceptCtrl', ['$scope', '$http', '$uibModalInstance', function($scope, $http, $uibModalInstance) { 
-    
-//     // Closes biography modal when close button is clicked
-//     $scope.close = function () {
-//         $uibModalInstance.dismiss('close');
-//     };
-// }])
+    // Closes Concept Modal when close button is clicked
+    $scope.close = function () {
+        $uibModalInstance.dismiss('close');
+    };
+}])
